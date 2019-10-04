67d57aee76d5d20484c6a143f2e2d8c4d01556c0
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2019-10-04 18:45:33 -0400

    Update dock.py

--- dock.py
+++ dock.py
@@ -7,7 +7,6 @@ Created on Fri Oct  4 18:20:49 2019
 from main_np import cline
 import pandas as pd 
 
-parser = argparse.ArgumentParser()
 df = pd.read_csv('/home/mcb/jboitr/data/moses_test.csv', nrows = 100)
 
 smiles = df['can']
