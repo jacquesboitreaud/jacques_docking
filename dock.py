bef6428aca080538168a74e6a2f7b3ad0d31b4ab
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2019-10-04 18:46:03 -0400

    Update dock.py

--- dock.py
+++ dock.py
@@ -4,7 +4,7 @@ Created on Fri Oct  4 18:20:49 2019
 
 @author: jacqu
 """
-from main_np import cline
+from main_np import main
 import pandas as pd 
 
 df = pd.read_csv('/home/mcb/jboitr/data/moses_test.csv', nrows = 100)
