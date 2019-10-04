f45a8acf73a1025f2882ed35dda3c2a00504ee37
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2019-10-04 16:45:06 -0400

    Update boxgrid.py

--- scripts/boxgrid.py
+++ scripts/boxgrid.py
@@ -39,8 +39,8 @@ distance_dielectric                       yes
 dielectric_factor                         4
 bump_filter                               yes
 bump_overlap                              0.75
-receptor_file                             {add_suffix_new_path(pdb_path, write_dir, '_rec_withH.mol2')}
-box_file                                  {add_suffix_new_path(pdb_path, write_dir, '.box.pdb')}
+receptor_file                             {os.path.join(write_dir, '_rec_withH.mol2')}
+box_file                                  {os.path.join(pdb_path, write_dir, '.box.pdb')}
 vdw_definition_file                       {params_path}/vdw_AMBER_parm99.defn
 score_grid_prefix                         {osp.join(write_dir,'grid')}
 """
