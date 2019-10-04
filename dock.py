a52eba8dc77830ddea9d952d701f69af118982ca
jacquesboitreaud <jacquesboitreaud@users.noreply.github.com>
2019-10-04 18:47:57 -0400

    Update dock.py

--- dock.py
+++ dock.py
@@ -6,10 +6,16 @@ Created on Fri Oct  4 18:20:49 2019
 """
 from main_np import main
 import pandas as pd 
+import argparse 
+
 
 df = pd.read_csv('/home/mcb/jboitr/data/moses_test.csv', nrows = 100)
 
 smiles = df['can']
 
-main(smiles = smiles)
+parser=argparse.ArgumentParser()
+parser.add_argument("-s", "--smiles", default='c1ccccc1', help="SMILES string of ligand to dock")
+args = parser.parse_args()
+
+main(args)
 
